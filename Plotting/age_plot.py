2e5df91618b3cbc2ec26db41f9005bf5613987ef
mattgo42 <47105768+mattgo42@users.noreply.github.com>
2019-01-28 14:02:16 +0000

    Python 3 compatible
    Added parentheses when needed
    Removed unnecessary diagnostics

--- Plotting/age_plot.py
+++ Plotting/age_plot.py
@@ -10,10 +10,8 @@ if __name__ == "__main__":
     H_0 = 73.2
     #Calculate Hubble Constant H_0 in units of yr^-1
     H_0_yrs = H_0*((31557600)*(10**3)/(3.08567758*10**22)) #s yr^-1 * km m^-1 *
-    print 1/H_0_yrs
 
     omega_m = np.arange(0.01, 1, 0.05)   #range of omega_m 0<omega_m<1
-    #t_0 = (10**11)*(1/(1-omega_m)**0.5)*np.log((1+(1-omega_m)**(0.5))/(omega_m)**0.5)
     t_0 = (2.0/3.0)*(1/H_0_yrs)*(1/(1-omega_m)**0.5)*np.log((1+(1-omega_m)**(0.5))/(omega_m)**0.5) #expression for age of universe
 
     #label x and y values
