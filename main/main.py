43aa4f28fd19abd6d3fbbb82aabb3e68130ff207
pabilbado <pablobilbao.25@gmail.com>
2019-02-03 17:29:11 +0000

    Main folder has been deleted as all it's 'relevant' content has been reorganised with the rest of the project

--- main/main.py
+++
@@ -1,9 +0,0 @@
-import integrator.trapezium as multi
-from data.getdata import data_as_list
-from functions.sfq import *
-
-
-initsfq = sfq_integral()  #initiates the sfq_integral class from sfq.py module.
-print(initsfq.cal())  #prints out the integral from sfq.py module.
-# initdata = data_as_list()
-# print(initdata.datalist())
