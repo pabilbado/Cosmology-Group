1679a5a166fb70d0bda7d4da287a72e3c0972deb
mattgo42 <47105768+mattgo42@users.noreply.github.com>
2019-02-05 15:36:00 +0000

    Latest version
    Shows 1-sigma and 2-sigma bounds on t_0, omega_m in red/yellow lines (boxes are in commented out code)
    
    Has a different colour cycle specified, though this does not seem to be working

--- plot_age_universe.py
+++ plot_age_universe.py
@@ -3,20 +3,22 @@ Purpose: plot the age of the universe as a function of omega_m for a range of
          w_x values
 
 @authors Pablo Bilbao, Matthew Gorton
-@version 2.0
+@version 3.0
 created 31 January 2019
-updated 2 February 2019
+updated 5 February 2019
 
 """
 
 from integrator.trapezium import trapeziumrule as integrate
 from functions.t0_part1b import t0_part1b as t0
+import matplotlib as mpl
 import matplotlib.pyplot as plt
-from matplotlib.collections import PatchCollection
-from matplotlib.patches import Rectangle
 import numpy as np
 
 fig, ax = plt.subplots()
+
+#Set the default color cycle
+mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=['b', 'cyan', 'lime', 'g'])
 
 #create function
 tfunc = t0()
@@ -27,7 +29,7 @@ wx_legend = ([])
 #produce a plot for each w_x value
 for x in np.arange(-.9,-.3, 0.2):
     tfunc.update(wx = x)
-    tfunc.plot(ax, [0.1,.9], 0.1)
+    tfunc.plot(ax, [0.01,.99], 0.01)
     #add label to legend, with the appropriate w_x value
     #use np.around to round w_x values to 1DP
     wx_legend = np.append(wx_legend, ['$w_{x}$ = ' + str(np.around(x, decimals = 1))])
@@ -70,44 +72,46 @@ omega_max_1sigma = omega_m_obs + omega_m_error
 #calculate upper and lower bounds of omega_m to 2-sigma
 omega_min_2sigma = omega_m_obs - 2*omega_m_error
 omega_max_2sigma = omega_m_obs + 2*omega_m_error
-
+"""
 #create error patches: ((x,y of bottom left corner), width, height, angle **kwargs)
 t_rectangle_1sigma = Rectangle((0, t_min_1sigma), 1, 2*t_obs_error, 0, alpha = 0.5, color = 'r')
 t_rectangle_2sigma = Rectangle((0, t_min_2sigma), 1, 4*t_obs_error, 0, alpha = 0.25, color = 'r')
 
 omega_rectangle_1sigma = Rectangle((omega_min_1sigma, 7.5), 2*omega_m_error, 10, alpha = 0.5, color = 'y')
 omega_rectangle_2sigma = Rectangle((omega_min_2sigma, 7.5), 4*omega_m_error, 10, alpha = 0.25, color = 'y')
-
-
+"""
+"""
+errorboxes = [t_rectangle_1sigma, t_rectangle_2sigma, omega_rectangle_1sigma, omega_rectangle_2sigma]
+for i in range(len(errorboxes)):
+    ax.add_artist(errorboxes[i])
+"""
 # Create patch collection
-pc = PatchCollection([t_rectangle_1sigma, t_rectangle_2sigma, omega_rectangle_1sigma, omega_rectangle_2sigma])
+#pc = PatchCollection([t_rectangle_1sigma, t_rectangle_2sigma, omega_rectangle_1sigma, omega_rectangle_2sigma])
+
+
+
 # Add collection to axes
-ax.add_collection(pc)
+#ax.add_collection(pc)
 
 
 #plot line showing upper and lower 1-sigma bound on t_0
-ax.plot([0.0,1.0],[t_min_1sigma, t_min_1sigma], color='#000000', linestyle='--', linewidth=1)
-ax.plot([0.0,1.0],[t_max_1sigma, t_max_1sigma], color='#000000', linestyle='--', linewidth=1)
-
-#wx_legend = np.append(wx_legend, ['$t_{0} (1 \sigma$ error)'])
+ax.plot([0.0,1.0],[t_min_1sigma, t_min_1sigma], color='r', alpha = 1, linestyle='--', linewidth=1)
 
 #plot line showing upper and lower 1-sigma bound on t_0
-ax.plot([0.0,1.0],[t_min_2sigma, t_min_2sigma], color='#B0C4DE', linestyle='--', linewidth=1)
-ax.plot([0.0,1.0],[t_max_2sigma, t_max_2sigma], color='#B0C4DE', linestyle='--', linewidth=1)
-
-#wx_legend = np.append(wx_legend, ['$\Omega_{m,0} (1 \sigma$ error)'])
+ax.plot([0.0,1.0],[t_min_2sigma, t_min_2sigma], color='r', alpha = 0.5, linestyle='--', linewidth=1)
 
 #plot line showing upper and lower 1-sigma bound on omega_m
-ax.axvline(x=omega_min_1sigma, ymin=0.0, ymax=1.0, color='#778899', linestyle='--', linewidth=1)
-ax.axvline(x=omega_max_1sigma, ymin=0.0, ymax=1.0, color='#778899', linestyle='--', linewidth=1)
-
-#wx_legend = np.append(wx_legend, ['$t_{0} (2 \sigma$ error)'])
+ax.axvline(x=omega_min_1sigma, ymin=0.0, ymax=1.0, color='k', alpha = 1, linestyle='--', linewidth=1)
 
 #plot line showing upper and lower 2-sigma bound on omega_m
-ax.axvline(x=omega_min_2sigma, ymin=0.0, ymax=1.0, color='#B0C4DE', linestyle='--', linewidth=1)
-ax.axvline(x=omega_max_2sigma, ymin=0.0, ymax=1.0, color='#B0C4DE', linestyle='--', linewidth=1)
+ax.axvline(x=omega_min_2sigma, ymin=0.0, ymax=1.0, color='k', alpha = 0.5, linestyle='--', linewidth=1)
 
-#wx_legend = np.append(wx_legend, ['$\Omega_{m,0} (2 \sigma$ error)'])
+wx_legend = np.append(wx_legend, ['$t_{0} (1 \sigma$ bound)', '$t_{0} (2 \sigma$ bound)', '$\Omega_{m,0} (1 \sigma$ bound)', '$\Omega_{m,0} (2 \sigma$ bound)'])
+
+ax.plot([0.0,1.0],[t_max_1sigma, t_max_1sigma], color='r', alpha = 1, linestyle='--', linewidth=1)
+ax.plot([0.0,1.0],[t_max_2sigma, t_max_2sigma], color='r', alpha = 0.5, linestyle='--', linewidth=1)
+ax.axvline(x=omega_max_1sigma, ymin=0.0, ymax=1.0, color='k', alpha = 1, linestyle='--', linewidth=1)
+ax.axvline(x=omega_max_2sigma, ymin=0.0, ymax=1.0, color='k', alpha = 0.5, linestyle='--', linewidth=1)
 
 #create legend
 plt.legend(wx_legend)
