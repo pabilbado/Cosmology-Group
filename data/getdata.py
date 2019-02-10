24ebe44cfb5d50cc9ca8c044a55fbad2069d2767
pabilbado <pablobilbao.25@gmail.com>
2019-02-10 12:48:29 +0000

    Added module to load the data in a format that works for the CPL calculation

--- data/getdata.py
+++ data/getdata.py
@@ -1,9 +1,11 @@
 import pandas as pd
+from functions.dCPL import dBAO
+db = dBAO()
 
 
-def datalist():
+def datalist(name= "data"):
 
-    d = pd.read_csv('data/data.csv')   #gets all the data from a csv file from an absolute path to directory.
+    d = pd.read_csv('data/{0}.csv'.format(name))   #gets all the data from a csv file from an absolute path to directory.
     df = pd.DataFrame(d)    #obtains dataframe; allows labelled columns and indexed items to be read and passed.
     dfToList = df['z'].tolist() #shifts all the values under the column named z to a list
     z = df["z"].tolist()
@@ -11,3 +13,10 @@ def datalist():
     theta = df["theta"].tolist()
     dic = {"z" : z, "a":a, "theta" : theta}
     return dic
+
+def obtaindata(filename):
+    data = datalist(name="{}".format(filename))
+    inpdata = []
+    for n, z in enumerate(data["z"]):
+        inpdata.append([z, db.cal(data["theta"][n])])
+    return inpdata
