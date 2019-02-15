d261c560c1ffc6fb730bbcf08e812ae2cfd33c14
mattgo42 <47105768+mattgo42@users.noreply.github.com>
2019-02-15 10:15:02 +0000

    Updated version
    Includes .update method to make plotting easier

--- functions/q_modgrav.py
+++ functions/q_modgrav.py
@@ -10,16 +10,23 @@ class q_modgrav(Function):
     def __init__(self):
         Function.__init__(self)
 
+    #update Method: Assigns a value of Omega_m_0 to q(z)
+    #           Inputs: value of Omega_m_0
+    #           Outputs: q(z) for a given Omega_m_0
+
+    def update(self, omeM):
+        self.omeM = omeM
+
     #cal Method: Calculates H(a)
-    #           Inputs: value of Omega_m_0, redshift z.
+    #           Inputs: redshift z.
     #           Outputs: the result of the function from given values.
-    def cal(self, omeM, z):
+    def cal(self, z):
 
-        q_numerator = 6 * omeM * (1+z)**3
+        q_numerator = 6 * self.omeM * (1+z)**3
 
-        q_firstterm = math.pow(((1-omeM)**2 + (4 * omeM * (1+z)**3)), 0.5)
+        q_firstterm = math.pow(((1-self.omeM)**2 + (4 * self.omeM * (1+z)**3)), 0.5)
 
-        q_secondterm = (1-omeM) + q_firstterm
+        q_secondterm = (1-self.omeM) + q_firstterm
 
         q = -1 + (q_numerator / (q_firstterm * q_secondterm)) #function q(z)
         return(q)
