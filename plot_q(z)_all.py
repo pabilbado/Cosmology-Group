5baeddfea35d996ef77e74653c55049ac12f7fa5
mattgo42 <47105768+mattgo42@users.noreply.github.com>
2019-02-18 15:45:55 +0000

    Corrected for part II

--- plot_q(z)_all.py
+++ plot_q(z)_all.py
@@ -1,6 +1,6 @@
 """
-Purpose: plots q(z) in a given redshift range, for the Lambda-CDM, SFW and DGP
-models
+Purpose: plots q(z) in a given redshift range, for the Lambda-CDM, SFQ-I, SFQ-II
+and pure mattermodels
 
 @author Matthew Gorton
 @version 1.5
@@ -9,15 +9,14 @@ models
 """
 import numpy as np
 import matplotlib.pyplot as plt
-from functions.q_modgrav import q_modgrav
-from functions.q_sfq_v2 import q_sfq
-from functions.q_LambdaCDM import q_LambdaCDM
+from functions.q_LambdaCDM import q_LambdaCDM as q1
+from functions.q_sfq_v2 import q_sfq as q2
 
 #create functions
-q1 = q_LambdaCDM()
-q2a = q_sfq()
-q2b = q_sfq()
-q3 = q_modgrav()
+q1 = q_1()
+q_purematter = q_1()
+q2a = q_2()
+q2b = q_2()
 
 #set value of omega_m,0 to best-fit Planck value
 omeM = 0.308
@@ -26,7 +25,9 @@ omeM = 0.308
 q1.update(omeM)
 q2a.update(omeM, 0.50)
 q2b.update(omeM, 0.23)
-q3.update(omeM)
+
+#for pure matter model, set omega_m,0 = 1
+q_purematter.update(1.)
 
 #range of z values
 rangz = np.arange(0., 5., 0.01)
@@ -51,13 +52,13 @@ plt.ylabel('q(z)')                 #label y axis
 
 #plot each q value and add appropriate description to legend
 q1.plot(ax, [0.0, 5.0], 0.01)
-wx_legend = np.append(wx_legend, ['$\Lambda$-CDM Model'])
+wx_legend = np.append(wx_legend, ['$\\Lambda$-CDM Model'])
 q2a.plot(ax, [0.0, 5.0], 0.01)
 wx_legend = np.append(wx_legend, ['SFQ Model 1 $(a_{\\tau} = 0.50)$'])
 q2b.plot(ax, [0.0, 5.0], 0.01)
 wx_legend = np.append(wx_legend, ['SFQ Model 1 $(a_{\\tau} = 0.23)$'])
-q3.plot(ax, [0.0, 5.0], 0.01)
-wx_legend = np.append(wx_legend, ['DGP Model'])
+q_purematter.plot(ax, [0.0, 5.0], 0.01)
+wx_legend = np.append(wx_legend, ['Pure Matter Model'])
 
 #add horizontal line at q=0
 ax.axhline(xmin=0.0, xmax=5.0, y=0.0, color='k', alpha = 0.5, linestyle='--', linewidth=1)
@@ -67,6 +68,9 @@ plt.gca().invert_xaxis()
 
 #create legend
 plt.legend(wx_legend)
+
+#remove white space
+fig.tight_layout()
 
 #save figure to a file
 fig.savefig('plot_q(z).pdf')
