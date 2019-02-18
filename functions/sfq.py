78a33828dc5a08dd823138bfd05dfd566693ffc1
Alice Cheng <alice.st.cheng@gmail.com>
2019-02-18 13:30:02 +0000

    updated update with various values

--- functions/sfq.py
+++ functions/sfq.py
@@ -11,12 +11,12 @@ later stages.
 class sfq_integrand(Function):
     def __init__(self):
         Function.__init__(self)
-
+        self.w_x = 1
 #cal Method: Calculates the function inside the integral.
 #           Inputs:  value of a.
 #           Outputs: the result of the function from given values.
     def cal(self, a):
-        w = -1 #(-1 + (self.wpas - self.wf)/(1 + math.pow(a/self.at, 1./self.tau))) #function for the barotropic parameter in terms of a
+        w = -1 + self.w_x*((self.wpas - self.wf)/(1 + math.pow(a/self.at, 1./self.tau))) #function for the barotropic parameter in terms of a
         intgrd = -(3.*(1+w)/a)  #the full integrand
         return(intgrd)
 
@@ -24,6 +24,7 @@ class sfq_integrand(Function):
 sfq_integral class: using given values for a sfq model, this class is responsible
 for integrating a given function for a value of redshifts, z. (If you want a list use the wrapper in integrator.mapper.map)
 """
+
 
 class sfq_integral(Function):
     def __init__(self):
@@ -48,13 +49,13 @@ class intdsfq(Function):
         self.rho = sfq_integral()
         self.wpas = 0
         self.wf = -1
-        self.z = 0
 
     def cal(self, a):   #calculates the integral for d(z). The integrand is long, and is therefore
-                        #broken down into the constant, and the square root using variables.
+        z = 1./(a)-1        #broken down into the constant, and the square root using variables.
         const = (self.c*self.a0)/(a**2)
-        sqrt_part = math.sqrt(self.omeM*math.pow(self.a0/a, 3) + self.omeX*self.rho.cal(self.z))
+        sqrt_part = math.sqrt(self.omeM*math.pow(self.a0/a, 3) + self.omeX*self.rho.cal(z))
         integrand = const*(1/(self.H0_2*sqrt_part))
+
 
         return integrand
 
@@ -67,6 +68,30 @@ class dsfq(Function):
         a = 1/(1+z)
         return integrate(self.integrand, [a, self.a0], 0.001)
 
-    def update(self, z = False):
-        if type(z) != type(False):
-            self.integrand.z = z
+    def update(self,
+                     w_x=False,
+                     omeM=False,
+                     omeX=False,
+                     at=False,
+                     tau=False,
+                     wp = False,
+                     wm = False,
+                     wf = False
+                     ):
+
+        if type(w_x) != type(False):
+            self.integrand.rho.integral.w_x = w_x
+        if type(omeM) != type(False):
+            self.integrand.omeM = omeM
+        if type(omeX) != type(False):
+            self.integrand.omeX = omeX
+        if type(at) != type(False):
+            self.integrand.at = at
+        if type(tau) != type(False):
+            self.integrand.tau = tau
+        if type(wp) != type(False):
+            self.integrand.wp = wp
+        if type(wm) != type(False):
+            self.integrand.wm = wm
+        if type(wf) != type(False):
+            self.integrand.wf = wf
