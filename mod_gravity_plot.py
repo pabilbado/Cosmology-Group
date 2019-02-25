0f97111008307b05ec76c0e8f96aa8a43c129cb6
mattgo42 <47105768+mattgo42@users.noreply.github.com>
2019-02-25 15:40:41 +0000

    Plots dL(z) for DGP and Lambda-CDM model

--- mod_gravity_plot.py
+++ mod_gravity_plot.py
@@ -1,12 +1,33 @@
 import matplotlib.pyplot as plt
-from functions.dL_PartIII import dL
+import numpy as np
+from functions.sfq import dL as dLSFQ
+from functions.dL_PartIII import dL as dLDGP
 
+dL_DGP = dLDGP()
+dL_LambdaCDM = dLSFQ()
+dL_LambdaCDM.update(factor=0)
 
-d_l = dL()
+#create legend
+legend = ([])
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(4,3))
 
-d_l.plot(ax, [0,2], 0.05)
+#set x and y axis limits
+ax.set_xlim(left=0.0, right=2.0)
 
-fig.show()
-input()
+plt.xlabel('Redshift z')           #label x axis
+plt.ylabel('Luminosity Distance $d_{L}$/Mpc') #label y axis
+
+dL_DGP.plot(ax, [0,2], 0.05)
+legend = np.append(legend, ['DGP Model'])
+dL_LambdaCDM.plot(ax, [0,2], 0.05)
+legend = np.append(legend, ['$\\Lambda$CDM Model'])
+
+#create legend
+plt.legend(legend)
+
+#remove white space
+fig.tight_layout()
+
+#save figure to a file
+fig.savefig('plot_dL_Pt3.pdf')
