ed912c9717ea9c28f6d289bce3938fef74192c4d
Alice Cheng <alice.st.cheng@gmail.com>
2019-02-25 09:17:08 +0000

    agg_accuracy_wFirst.py

--- agg_accuracy_wFirst.py
+++ agg_accuracy_wFirst.py
@@ -18,21 +18,21 @@ dcdm.update(factor=0)
 #calculate d_L for each model at redshift z=0.5
 d_L1 = dsfq1.cal(z1)
 d_L2 = dsfq2.cal(z1)
-d_lamcdm = dCDM.cal(z1)
+d_lamcdm = dcdm.cal(z1)
 #find aggregate accuracy for SFQ-I model
-aggy1 = math.abs((d_L1-d_lamcdm)/d_L1)
-print('sfq1 (z = ' + z1 + '):' + aggy1)
+aggy1 = abs((d_L1-d_lamcdm)/d_L1)
+print('sfq1 (z = ' + str(z1) + '):' + str(aggy1))
 #find aggregate accuracy for SFQ-I model
-aggy2 = math.abs((d_L2-d_lamcdm)/d_L2)
-print('sfq2 (z = ' + z1 + '):' + aggy2)
+aggy2 = abs((d_L2-d_lamcdm)/d_L2)
+print('sfq2 (z = ' + str(z1) + '):' + str(aggy2))
 
 #calculate d_L for each model at redshift z=1.32
 d_L1 = dsfq1.cal(z2)
 d_L2 = dsfq2.cal(z2)
-d_lamcdm = dCDM.cal(z2)
+d_lamcdm = dcdm.cal(z2)
 #find aggregate accuracy for SFQ-I model
-aggy1 = math.abs((d_L1-d_lamcdm)/d_L1)
-print('sfq1 (z = ' + z2 + '):' + aggy1)
+aggy1 = abs((d_L1-d_lamcdm)/d_L1)
+print('sfq1 (z = ' + str(z2) + '):' + str(aggy1))
 #find aggregate accuracy for SFQ-II model
-aggy2 = math.abs((d_L2-d_lamcdm)/d_L2)
-print('sfq2 (z = ' + z2 + '):' + aggy2)
+aggy2 = abs((d_L2-d_lamcdm)/d_L2)
+print('sfq2 (z = ' + str(z2) + '):' + str(aggy2))
